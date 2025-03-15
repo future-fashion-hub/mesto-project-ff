c3f2addec95a782410a958efe9ee37e6ad4acb90
Danila Vechernin <d.vechernin03@gmail.com>
2025-03-15 18:16:59 +0300

    после правок

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,18 @@ const templateCard = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const cardList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-function createCard(cardData) {
+function createCard(cardData, deleteCard) {
   const card = templateCard.cloneNode(true);
   const cardImage = card.querySelector('.card__image');
   const cardTitle = card.querySelector('.card__title');
   const cardDeleteButton = card.querySelector('.card__delete-button');
 
   cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
   cardTitle.textContent = cardData.name;
 
-  cardDeleteButton.addEventListener('click', () => removeCard(cardDeleteButton));
+
+  cardDeleteButton.addEventListener('click', () => deleteCard(cardDeleteButton));
   return card;
 };
 // @todo: Функция удаления карточки
@@ -22,7 +24,7 @@ function removeCard(item) {
 };
 // @todo: Вывести карточки на страницу
 initialCards.forEach(item => {
-  const newCard = createCard(item);
+  const newCard = createCard(item, removeCard);
   cardList.appendChild(newCard);
 });
 
